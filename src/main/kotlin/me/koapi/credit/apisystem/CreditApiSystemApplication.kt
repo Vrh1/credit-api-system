d53ce7abe25104ae478cc5ae334e288ec8a69042
Vergara <jvv10.vergara@gmail.com>
2023-10-24 11:59:47 -0300

    Initial commit

---
+++ src/main/kotlin/me/koapi/credit/apisystem/CreditApiSystemApplication.kt
@@ -0,0 +1,11 @@
+package me.koapi.credit.apisystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApiSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApiSystemApplication>(*args)
+}
---
+++ src/test/kotlin/me/koapi/credit/apisystem/CreditApiSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.koapi.credit.apisystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApiSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
