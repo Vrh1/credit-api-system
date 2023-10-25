749cb115954276be0dc407adacd7eda7db59df45
Vergara <jvv10.vergara@gmail.com>
2023-10-25 10:06:08 -0300

    Entitys
    Criação das Entitys

---
+++ src/main/kotlin/me/koapi/credit/apisystem/ennumeration/Status.kt
@@ -0,0 +1,7 @@
+package me.koapi.credit.apisystem.ennumeration
+
+enum class Status {
+    IN_PROGRESS,
+    APPROVED,
+    REJECT
+}
---
+++ src/main/kotlin/me/koapi/credit/apisystem/entity/Adress.kt
@@ -0,0 +1,10 @@
+package me.koapi.credit.apisystem.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Adress (
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
+)
